e1aa8363a345c0c84e85b7fe920acecd005ebc70
Lojain_hussien <lojyhussien092@gmail.com>
2025-05-06 23:40:30 +0200

    prodcuts edits
    add product home , add folders for old games , kantine and library

---
+++ HTML/kantine products/products.js
@@ -0,0 +1,52 @@
+let quantity = 1;
+
+function increase() { //lma el user ydos 3la (+) el ra2m yzed
+    quantity++;
+    document.getElementById('quantity').textContent = quantity;
+}
+
+function decrease() {
+    if (quantity > 1) { // 3lshan el ra2m my2lsh le el zero
+        quantity--; //lma el user ydos 3la (-) el ra2m y2l
+        document.getElementById('quantity').textContent = quantity;
+    }
+}
+
+const sectionStates = {
+    1: 1,
+    2: 1
+};
+function showNextImages(sectionId) {
+    document.getElementById(`product-row1-${sectionId}`).classList.add("hidden");
+    document.getElementById(`product-row2-${sectionId}`).classList.remove("hidden");
+    sectionStates[sectionId] = 2;
+    updateButtonHighlight(sectionId);
+}
+
+function showPrevImages(sectionId) {
+    document.getElementById(`product-row2-${sectionId}`).classList.add("hidden");
+    document.getElementById(`product-row1-${sectionId}`).classList.remove("hidden");
+    sectionStates[sectionId] = 1;
+    updateButtonHighlight(sectionId);
+}
+
+function updateButtonHighlight(sectionId) {
+    const section = document.querySelector(`.product-section[data-id="${sectionId}"]`);
+    const leftBtn = section.querySelector('.product-left');
+    const rightBtn = section.querySelector('.product-right');
+
+    if (sectionStates[sectionId] === 1) {
+        rightBtn.classList.add("highlighted");
+        leftBtn.classList.remove("highlighted");
+    } else {
+        leftBtn.classList.add("highlighted");
+        rightBtn.classList.remove("highlighted");
+    }
+}
+
+window.onload = function () {
+    for (let id in sectionStates) {
+        updateButtonHighlight(id);
+    }
+};
+
