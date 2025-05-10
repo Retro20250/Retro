110b480d5d38e388c0cfa52e4bd44123a9194a07
mariamhany911 <mariamhany9112006@gmail.com>
2025-05-11 00:13:40 +0300

    ya akhy ana GADEEET

--- All Download/Home/Home.js
+++ All Download/Home/Home.js
@@ -7,12 +7,9 @@ document.getElementById("sm-btn").addEventListener("click", function () {
 
 
 
-
-
 const menuButton = document.querySelector('.menu-button');
 const dropdownContent = document.querySelector('.dropdown-content');
 
 menuButton.addEventListener('click', () => {
   dropdownContent.classList.toggle('show'); // إضافة أو إزالة الكلاس "show"
 });
-
