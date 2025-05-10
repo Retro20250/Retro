da8a8533c932d2cda9ca5e4a4eccbfa0d44f6aa6
ziadd552 <ziadtarek847@gmail.com>
2025-05-10 15:31:06 +0300

    صصصص

--- All Download/Home/Home.js
+++ All Download/Home/Home.js
@@ -5,3 +5,14 @@ document.getElementById("sm-btn").addEventListener("click", function () {
   document.getElementById("popup").style.display = "block";
 });
 
+
+
+
+
+const menuButton = document.querySelector('.menu-button');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+menuButton.addEventListener('click', () => {
+  dropdownContent.classList.toggle('show'); // إضافة أو إزالة الكلاس "show"
+});
+
